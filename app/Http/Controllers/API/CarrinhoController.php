4a64718b9f2d620cec1b9748005f25796eb8a52f
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2022-01-23 20:47:36 -0300

    feat(): alteraçoes carrinho

--- app/Http/Controllers/API/CarrinhoController.php
+++ app/Http/Controllers/API/CarrinhoController.php
@@ -9,12 +9,14 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\Cart;
 use App\Http\Resources\CategoriesResource;
+use Illuminate\Support\Facades\DB;
 
 class CarrinhoController extends Controller
 {
 
     private $rules = [
         'user_id' => 'required|integer',
+        'product_id' => 'required|integer',
     ];
 
     /**
@@ -26,7 +28,7 @@ class CarrinhoController extends Controller
     {
         $data = Category::latest()->get();
         return response()->json(['Carrinhos encontrados:', CategoriesResource::collection($data)]);
-    } */
+    }
 
     public function cart(Request $request)
     {
@@ -67,136 +69,174 @@ class CarrinhoController extends Controller
      */
     public function store(Request $request)
     {
-        if (!$request->product_id) {
 
-            return response()->json(["error" => "Requisição com corpo incompleto"], 400);
+        $validator = Validator::make($request->all(), $this->rules);
+
+        if($validator->fails()){
+            return response()->json(
+                ["message" => "Ocorreu um erro nos dados informados."]
+            );
         }
 
-        $prod = Product::all()->where('id', $request->product_id)->count();
+        // verificando se usuário ainda tem carrinho ativo
+        $carrinhoAtivo = DB::table('cart')
+            ->where([
+                ['user_id', $request->user_id],
+                ['status', 1]
+            ])
+            ->first();
 
-        if ($prod > 0) {
-
-            //$user = auth()->user()->id;
-
-            $checkCart = Cart::all()->where('user_id', $request->user_id)->where('product_id', $request->product_id)->first();
-
-            if ($checkCart) {
-
-                $amount = $checkCart->amount;
-
-                $checkCart->update(['amount' => $amount + 1]);
-
-                return response()->json(["success" => "Produto adicionado no carrinho"]);
-            } else {
-
-                Cart::create([
-                    'user_id' => $request->user_id,
-                    'product_id' => $request->product_id,
-                    'amount' => $request->amount
-                ]);
-    
-                return response()->json(["success" => "Produto adicionado no carrinho"]);
-            }
-
-        } else {
+        if($carrinhoAtivo){
+            DB::table('cart_product')->insert([
+                'cart_id' => $carrinhoAtivo->id,
+                'product_id' => $request->product_id,
+                'amount' => 1
+            ]);
 
             return response()->json([
-                "error" => "Este Produto não existe na base de dados",
-                "product_id" => "Is invalid",
-                "mesage" => "Impossível adicionar no carrinho"
-            ], 401);
-        }
-    }
-
-
-
-
-    public function removeProdOne(Request $request)
-    {
-
-        if (!$request->product_id) {
-
-            return response()->json(["Error" => "Dados incompletos"], 400);
-        }
-
-        //$user = auth()->user()->id;
-
-        $checkCart = Cart::all()->where('user_id', $request->user_id)->where('product_id', $request->product_id)->first();
-
-        if ($checkCart == null) {
-
-            return response()->json(["Error" => "Carrinho não encontrado"], 404);
-        }
-
-        if ($checkCart->amount > 1) {
-
-            $amount = $checkCart->amount;
-
-            $checkCart->update(['amount' => $amount - 1]);
-
+                "message" => "Produto adicionado ao carrinho."
+            ]);
         } else {
+            $cartId = DB::table('cart')->insertGetId([
+                'user_id' => $request->user_id,
+                'status' => 1
+            ]);
 
-            $checkCart->delete();
+            DB::table('cart_product')->insert([
+                'cart_id' => $cartId,
+                'product_id' => $request->product_id,
+                'amount' => 1
+            ]);
+
+            return response()->json([
+                "message" => "Produto adicionado ao carrinho."
+            ]);
         }
-        $product = Product::findOrFail($checkCart->product_id);
-        return response()->json(["success" => "{$product->name} uma unidade removida com sucesso"]);
+
+        // return response()->json($carrinhoAtivo);
+
+        // $product = Product::create($request->all());
+
+        // return response()->json(new ProductsResource($product));
+
+        // if (!$request->product_id) {
+
+        //     return response()->json(["error" => "Requisição com corpo incompleto"], 400);
+        // }
+
+        // $prod = Product::all()->where('id', $request->product_id)->count();
+
+        // if ($prod > 0) {
+
+        //     //$user = auth()->user()->id;
+
+        //     $checkCart = Cart::all()->where('user_id', $request->user_id)->where('product_id', $request->product_id)->first();
+
+        //     if ($checkCart) {
+
+        //         $amount = $checkCart->amount;
+
+        //         $checkCart->update(['amount' => $amount + 1]);
+
+        //         return response()->json(["success" => "Produto adicionado no carrinho"]);
+        //     } else {
+
+        //         Cart::create([
+        //             'user_id' => $request->user_id,
+        //             'product_id' => $request->product_id,
+        //             'amount' => $request->amount
+        //         ]);
+
+        //         return response()->json(["success" => "Produto adicionado no carrinho"]);
+        //     }
+
+        // } else {
+
+        //     return response()->json([
+        //         "error" => "Este Produto não existe na base de dados",
+        //         "product_id" => "Is invalid",
+        //         "mesage" => "Impossível adicionar no carrinho"
+        //     ], 401);
+        // }
     }
 
+    // public function removeProdOne(Request $request)
+    // {
+
+    //     if (!$request->product_id) {
+
+    //         return response()->json(["Error" => "Dados incompletos"], 400);
+    //     }
+
+    //     //$user = auth()->user()->id;
+
+    //     $checkCart = Cart::all()->where('user_id', $request->user_id)->where('product_id', $request->product_id)->first();
+
+    //     if ($checkCart == null) {
+
+    //         return response()->json(["Error" => "Carrinho não encontrado"], 404);
+    //     }
+
+    //     if ($checkCart->amount > 1) {
+
+    //         $amount = $checkCart->amount;
+
+    //         $checkCart->update(['amount' => $amount - 1]);
+
+    //     } else {
+
+    //         $checkCart->delete();
+    //     }
+    //     $product = Product::findOrFail($checkCart->product_id);
+    //     return response()->json(["success" => "{$product->name} uma unidade removida com sucesso"]);
+    // }
+
+    // public function removeProd(Request $request)
+    // {
+    //     if (!$request->product_id) {
+
+    //         return response()->json(["Error" => "Dados incompletos"], 400);
+    //     }
+
+    //     //$user = auth()->user()->id;
+
+    //     $checkCart = Cart::all()->where('user_id', $request->user_id)->where('product_id', $request->product_id)->first();
+
+    //     if ($checkCart == null) {
+
+    //         return response()->json(["Error" => "Carrinho não encontrado"], 404);
+    //     }
+
+    //     if ($checkCart->amount > 0) {
+    //         $checkCart->delete();
+    //     }
+    //     $product = Product::findOrFail($checkCart->product_id);
+    //     return response()->json(["success" => "{$product->name} removido do carrinho com sucesso"]);
+    // }
+
+
+    // public function removeCart(int $user)
+    // {
+
+    //     //$user = auth()->user()->id;
+
+    //     $this->delete($user);
+
+    //     return response()->json(["success" => "Carrinho Vazio"]);
+    // }
 
 
 
+    // private function delete(int $user)
+    // {
 
-    public function removeProd(Request $request)
-    {
+    //     //remove produto do carrinho do cliente
+    //     $removeProducts = Cart::all()->where('user_id', $user);
 
-        if (!$request->product_id) {
-
-            return response()->json(["Error" => "Dados incompletos"], 400);
-        }
-
-        //$user = auth()->user()->id;
-
-        $checkCart = Cart::all()->where('user_id', $request->user_id)->where('product_id', $request->product_id)->first();
-
-        if ($checkCart == null) {
-
-            return response()->json(["Error" => "Carrinho não encontrado"], 404);
-        }
-
-        if ($checkCart->amount > 0) {
-            $checkCart->delete();
-        }
-        $product = Product::findOrFail($checkCart->product_id);
-        return response()->json(["success" => "{$product->name} removido do carrinho com sucesso"]);
-    }
-
-
-    public function removeCart(int $user)
-    {
-
-        //$user = auth()->user()->id;
-
-        $this->delete($user);
-
-        return response()->json(["success" => "Carrinho Vazio"]);
-    }
-
-
-
-    private function delete(int $user)
-    {
-
-        //remove produto do carrinho do cliente
-        $removeProducts = Cart::all()->where('user_id', $user);
-
-        foreach ($removeProducts as $products) {
-            $products->delete();
-        }
-    }
-
-
-
-
+    //     foreach ($removeProducts as $products) {
+    //         $products->delete();
+    //     }
+    // }
 
     /**
      * Display the specified resource.
@@ -206,11 +246,17 @@ class CarrinhoController extends Controller
      */
     public function show($id)
     {
-        $carrinho = Category::find($id);
-        if (is_null($carrinho)) {
-            return response()->json('Dados não encontrados.', 404);
-        }
-        return response()->json([new CategoriesResource($carrinho)]);
+        $produtosCarrinho = DB::table('cart')
+            ->leftJoin('cart_product', 'cart.id', '=', 'cart_product.cart_id')
+            ->leftJoin('products', 'cart_product.product_id', '=', 'products.id')
+            ->select('cart.user_id', 'cart_product.id as cart_product_id', 'cart_product.amount', 'products.*')
+            ->where([
+                ['cart.status', 1],
+                ['cart.user_id', $id]
+            ])
+            ->get();
+
+        return response()->json($produtosCarrinho);
     }
 
     /**
@@ -220,23 +266,23 @@ class CarrinhoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        $validator = Validator::make($request->all(), $this->rules);
+    // public function update(Request $request, $id)
+    // {
+    //     $validator = Validator::make($request->all(), $this->rules);
 
-        if($validator->fails()){
-            return response()->json($validator->errors());
-        }
+    //     if($validator->fails()){
+    //         return response()->json($validator->errors());
+    //     }
 
-        $carrinho = Category::findOrFail($id);
-        if($carrinho){
-            $carrinho->name = $request->name;
+    //     $carrinho = Category::findOrFail($id);
+    //     if($carrinho){
+    //         $carrinho->name = $request->name;
 
-            $carrinho->save();
+    //         $carrinho->save();
 
-            return response()->json(['carrinho atualizada com sucesso:', new CategoriesResource($carrinho)]);
-        }
-    }
+    //         return response()->json(['carrinho atualizada com sucesso:', new CategoriesResource($carrinho)]);
+    //     }
+    // }
 
     /**
      * Remove the specified resource from storage.
@@ -246,11 +292,20 @@ class CarrinhoController extends Controller
      */
     public function destroy($id)
     {
-        $carrinho = Category::findOrFail($id);
-        if($carrinho){
-            $carrinho->delete();
+        $produtosCarrinho = DB::table('cart')
+            ->leftJoin('cart_product', 'cart.id', '=', 'cart_product.cart_id')
+            ->leftJoin('products', 'cart_product.product_id', '=', 'products.id')
+            ->select('cart.user_id', 'cart_product.id as cart_product_id', 'cart_product.amount', 'products.*')
+            ->where([
+                ['cart.status', 1],
+                ['cart.user_id', $id]
+            ])
+            ->get();
+
+        if($produtosCarrinho){
+            DB::table('cart_product')->where('product_id', $id)->delete();
         }
 
-        return response()->json('carrinho excluída com sucesso.');
+        return response()->json('Produto excluído do carrinho com sucesso.');
     }
 }
