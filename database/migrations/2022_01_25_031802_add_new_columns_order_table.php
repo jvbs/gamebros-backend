02b129f4abeb5a853887e0796f74a8de314961ce
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2022-01-25 01:28:07 -0300

    feat(): pedidos

---
+++ database/migrations/2022_01_25_031802_add_new_columns_order_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnsOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('cart_id');
+            $table->decimal('total_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('cart_id');
+            $table->dropColumn('total_price');
+        });
+    }
+}
